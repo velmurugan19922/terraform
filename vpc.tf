b26865a146cd4967000715db80d9eb2aa6a4b407
velmurugan19922 <velmurugan19922@gmail.com>
2024-02-17 09:48:48 +0000

    to add public and private subnet

--- vpc.tf
+++ vpc.tf
@@ -14,7 +14,40 @@ resource "aws_subnet" "lms-public-sn-A" {
   vpc_id     = aws_vpc.lms-vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone ="ap-south-1a"
+  map_public_ip_on_launch = "true"
   tags = {
-    Name = "lms-public-subnet-a"
+    Name = "lms-public-subnet-A"
   }
 }
+# to create public subnet-B
+
+resource "aws_subnet" "lms-public-sn-B" {
+  vpc_id     = aws_vpc.lms-vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone ="ap-south-1a"
+  map_public_ip_on_launch = "true"
+  tags = {
+    Name = "lms-public-subnet-B"
+  }
+}
+# to create private subnet-A
+
+resource "aws_subnet" "lms-private-sn-A" {
+  vpc_id     = aws_vpc.lms-vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone ="ap-south-1b"
+  tags = {
+    Name = "lms-private-subnet-A"
+  }
+}
+# to create private subnet-A
+
+resource "aws_subnet" "lms-private-sn-B" {
+  vpc_id     = aws_vpc.lms-vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone ="ap-south-1b"
+  tags = {
+    Name = "lms-private-subnet-B"
+  }
+}
+
