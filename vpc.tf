ea755c364407d2f3ea686fb2522a25b7f38edb8d
velmurugan19922 <velmurugan19922@gmail.com>
2024-02-17 08:40:26 +0000

    add public subnet-a

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,20 @@
+# to create vpc
 resource "aws_vpc" "lms-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
     Name = "lms"
+  }
+}
+
+# to create public subnet-A
+
+resource "aws_subnet" "lms-public-sn-A" {
+  vpc_id     = aws_vpc.lms-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone ="ap-south-1a"
+  tags = {
+    Name = "lms-public-subnet-a"
   }
 }
